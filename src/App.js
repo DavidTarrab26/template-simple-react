8625d252ede8b9990be978077136ad427603f9b2
Davidtarrab <dudutarrab@gmail.com>
2021-12-30 20:21:26 -0300

    listo

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
 import Blog from './componentes/blog/Blog';
 import Contacto from './componentes/contacto/Contacto';
 import Inicio from './componentes/inicio/Inicio';
-import Notfound from './componentes/notfound/Notfound';
 
 function App() {
   return (
@@ -12,12 +11,9 @@ function App() {
     <Router>
       <Navbar />
         <Routes>
-          <Route path="" element={<Inicio />}/>
-          <Route path="/" element={<Inicio />}/>
           <Route path="/Inicio" element={<Inicio />}/>
           <Route path="/Blog" element={<Blog />}/>
           <Route path="/Contacto" element={<Contacto />}/>
-          <Route path="*" element={<Notfound />}/> 
         </Routes>
       <Footer />
     </Router>
